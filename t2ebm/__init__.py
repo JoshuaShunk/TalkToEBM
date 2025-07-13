a1c1f085e676968ca0dfa9dada79c4d250b5134d
JoshuaShunk <hockeyshunk04@gmail.com>
2025-07-13 18:35:43 -0400

    fix import error

--- t2ebm/__init__.py
+++ t2ebm/__init__.py
@@ -2,10 +2,7 @@
 TalkToEBM: A Natural Language Interface to Explainable Boosting Machines
 """
 
-# Import modules that need to be accessible
 from . import graphs, llm, prompts, utils
-
-# high-level functions
 from .functions import (
     describe_ebm,
     describe_graph,
